3ce950d4391270aef41974824769c3dd1fc75907
TheKingHueBR <contato.felipedelucca@outlook.com>
2016-03-17 13:28:12 -0300

    Added files via upload

---
+++ ban/Ban.java
@@ -0,0 +1,17 @@
+package ban;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Ban extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+}
